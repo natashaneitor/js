dcbd22eef33329906b123524d45eb3b6415c900c
Natasha <sara.nata.1314@gmail.com>
2022-10-04 15:00:55 -0300

    pending invert array

--- octubre/4/arrays.js
+++ octubre/4/arrays.js
@@ -1,12 +1,9 @@
 // Desafio 1
 const nambers = [2,38,9,27,8,9]
-const invertArray = (originalArray){
-    
-}
-
+const invertArray = (originalArray) 
 
 console.log(number)
 do {
-    console.log(NUMber[NUMber])
+    console.log(NUMber[NUMber])s
     number--
 } while (number >= 0)
